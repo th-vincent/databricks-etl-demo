19fc6fb0588cc0ee8e02f2a4722af6b7fb9a3b25
Theotime Vincent <theotimevincent@gmail.com>
2026-05-27 13:49:08 +0300

    KPI modif

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from etl.extract import extract
-from etl.transform import transform
+from etl.transform import transform, add_kpi
 from etl.load import load
 
 df = extract("data/sales.csv")
 df = transform(df)
+df = add_kpi(df)
 load(df)
--- transform.py
+++ transform.py
@@ -1,3 +1,7 @@
 def transform(df):
     df["amount_eur"] = df["amount"] * 0.93
     return df
+
+def add_kpi(df):
+    df["high_value"] = df["amount"] > 250
+    return df
